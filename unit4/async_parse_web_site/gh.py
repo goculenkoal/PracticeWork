3ac204fa7aaf246924cada7f91c011935d755264
alex <goculenko.al@gmail.com>
2024-07-29 21:20:15 +0300

    Delete garbage

--- unit4/async_parse_web_site/gh.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-def convert_seconds(seconds):
-    return str(datetime.timedelta(seconds=seconds))
-
-
-print(convert_seconds(484.11316776275635))
-print(convert_seconds(484.10208203100046))
